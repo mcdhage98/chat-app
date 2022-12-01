e236c8cfb1ad7ef59588a34768537202797f2908
mayurcdhage <mayurcdhage@users.noreply.github.com>
2022-12-01 20:30:20 +0530

    bar caht added

---
+++ src/Components/BarGraph/BarGraph.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+
+import "./BarGraph.css";
+
+export const UserData = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300,
+    userLost: 234,
+  },
+];
+
+export default function BarGraph() {
+  const [userData, setUserData] = useState({
+    labels: ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"],
+    datasets: [
+      {
+        label: "Activity",
+        data: [50, 90, 40, 50, 66, 38, 50],
+        backgroundColor: [
+          "#c8d8fe",
+          "#0f4cff",
+          "#c8d8fe",
+          "#c8d8fe",
+          "#c8d8fe",
+          "#c8d8fe",
+          "#c8d8fe",
+        ],
+        borderWidth: 0,
+        barThickness: 10,
+        borderRadius: 10,
+      },
+    ],
+  });
+
+  const options = {
+    scales: {
+      x: {
+        display: false,
+        gridLines: {
+          display: false,
+        },
+      },
+      y: {
+        display: false,
+        gridLines: {
+          display: false,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+  return (
+	<>
+      <div className="barGraphHeader">
+		<p className="barGraphLegend1">Current Week</p>
+		<p className="barGraphLegend2"> Activity</p>
+	  </div>
+
+      <Bar data={userData} options={options}></Bar>
+	  <div className="xAxis">
+		<p>MON</p>
+		<p className="active">TUE</p>
+		<p>WED</p>
+		<p>THU</p>
+		<p>FRI</p>
+		<p>SAT</p>
+		<p>SUN</p>
+	  </div>
+    </>
+  );
+}
--- src/Components/StatsContainer/StatsContainer.js
+++ src/Components/StatsContainer/StatsContainer.js
@@ -2,23 +2,46 @@ import React from "react";
 import Card from "../Card";
 import "./StatsContainer.css";
 import StatsCard from "../StatsCard/StatsCard";
-import Svg from "../../assets/svg"
+import Svg from "../../assets/svg";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import BarGraph from "../BarGraph/BarGraph";
 
-export default function StatsContainer(){
-	return(
-		<Card sx={{marginTop:"20px"}}>
-			<Grid2 container>
-				<Grid2 item xs={6}>
-			<StatsCard statColor="blue" statName="Time" statValue="13hr" StatIcon={Svg.ClockIcon}/>
-			<StatsCard statColor="purple" statName="Meetings" statValue="119" StatIcon={Svg.CalenderIcon}/>
- 			</Grid2>
-			<Grid2 item xs={6}>
-				<StatsCard statColor="green" statName="Attended" statValue="188" StatIcon={Svg.PeopleIcon}/>
-				<StatsCard statColor="orange" statName="Rejected" statValue="44" StatIcon={Svg.MessageRejectedIcon}/>
-			</Grid2>
-			</Grid2>
-			
-		</Card>
-	)
+export default function StatsContainer() {
+  return (
+    <Card sx={{ marginTop: "20px" }}>
+      <Grid2 container>
+        <Grid2 item xs={6}>
+          <StatsCard
+            statColor="blue"
+            statName="Time"
+            statValue="13hr"
+            StatIcon={Svg.ClockIcon}
+          />
+          <StatsCard
+            statColor="purple"
+            statName="Meetings"
+            statValue="119"
+            StatIcon={Svg.CalenderIcon}
+          />
+        </Grid2>
+        <Grid2 item xs={6}>
+          <StatsCard
+            statColor="green"
+            statName="Attended"
+            statValue="188"
+            StatIcon={Svg.PeopleIcon}
+          />
+          <StatsCard
+            statColor="orange"
+            statName="Rejected"
+            statValue="44"
+            StatIcon={Svg.MessageRejectedIcon}
+          />
+        </Grid2>
+        <Grid2 item xs={12}>
+          <BarGraph />
+        </Grid2>
+      </Grid2>
+    </Card>
+  );
 }
